e8fe7d6436d624a949674cd7190a07bbdaa1c832
russormes <russormes@gmail.com>
2014-02-05 14:08:22 -0500

    Initialise repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+A2p::Application.config.secret_key_base = 'd0b519f894afa5e854038281ec3b152e0afd12f32db0f48d44cf9463f0401b227a62aa4abedb676e0213e2279ca600c459a49dda5c90cc27e3ccc2e528d8019c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+A2p::Application.config.session_store :cookie_store, key: '_a2p_session'
