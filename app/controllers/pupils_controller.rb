ab3758f025b588a056c4644afb066a0c17524d76
leonormes <leonormes@gmail.com>
2014-02-16 21:51:02 +0000

    Adding destroy link

--- app/controllers/pupils_controller.rb
+++ app/controllers/pupils_controller.rb
@@ -7,12 +7,28 @@ class PupilsController < ApplicationController
   end
   
   def create
-  @pupil = Pupil.new(params[:post])
+  @pupil = Pupil.new(pupil_params)
   @pupil.save
   redirect_to @pupil
   end
   
+  def destroy
+  @pupil = Pupil.find(params[:id])
+  @pupil.destroy
+  redirect_to pupil_path
+end
+  
+  private
+  def pupil_params
+    params.require(:pupil).permit(:given_name, :family_name)
+  end
+  
+def show
+  @pupil = Pupil.find(params[:id])
+end
 
+
+  
   def import
     begin
       Pupil.import(params[:file])
@@ -20,5 +36,7 @@ class PupilsController < ApplicationController
     rescue
       redirect_to root_url, notice: "Invalid file format."
     end # end begin. 
+    
+ 
   end # end def import.
 end # end class.
--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ A2p::Application.routes.draw do
   end
  
   root to: "pupils#index"
-  
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
