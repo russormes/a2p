08ee98a91b9528f290043306e630b8743f75c1bc
Leon Ormes <leonormes@gmail.com>
2014-03-02 10:07:15 +0000

    Sign up route added and all test up to scratch. New user has password digest

--- db/schema.rb
+++ db/schema.rb
@@ -15,6 +15,7 @@ ActiveRecord::Schema.define(version: 20140228183019) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+  enable_extension "adminpack"
 
   create_table "groupings", force: true do |t|
     t.integer  "pupil_id"
