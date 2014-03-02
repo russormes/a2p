df2008337810c9c178840a00075bed21387ab984
Leon Ormes <leonormes@gmail.com>
2014-03-02 11:33:54 +0000

    Added all users page. Link to signup page.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,14 @@
 class UsersController < ApplicationController
+  
     def show
     @user = User.find(params[:id])
   end
-
+  
+  def index
+    @users = User.all
+  end
+  
   def new
   end
+  
 end
