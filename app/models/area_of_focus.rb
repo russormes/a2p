f5217d353e6c2948671c55e9b04d8c85e3bba50a
Leon Ormes <leonormes@gmail.com>
2014-03-03 09:53:16 +0000

    New models

--- app/models/area_of_focus.rb
+++ app/models/area_of_focus.rb
@@ -1,4 +1,5 @@
 class AreaOfFocus < ActiveRecord::Base
   belongs_to :discrete_areas_of_development
   has_many :statements_of_achievement
+  
 end
--- app/models/level.rb
+++ app/models/level.rb
@@ -1,2 +1,4 @@
 class Level < ActiveRecord::Base
+  belongs_to_many :discrete_areas_of_learning
+  has_many :areas_of_focus
 end
